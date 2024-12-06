94f10e091dc1a1eeec375f715d0174d679150dec
Alespic26 <alespicm4ddino@gmail.com>
2024-12-06 15:10:19 +0100

    Update SplineTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
@@ -4,6 +4,8 @@ import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.TankDrive;
@@ -11,9 +13,21 @@ import org.firstinspires.ftc.teamcode.TankDrive;
 public final class SplineTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
+        DcMotor angularArmDrive;
+        DcMotor extensionArmDrive;
+        Servo handArmServo;
+
+        angularArmDrive = hardwareMap.get(DcMotor.class, "angular_arm_drive");
+        extensionArmDrive = hardwareMap.get(DcMotor.class, "extension_arm_drive");
+        handArmServo = hardwareMap.get(Servo.class, "hand_arm_servo");
+
+        angularArmDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        extensionArmDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        
         Pose2d beginPose = new Pose2d(0, 0, 0);
         if (TuningOpModes.DRIVE_CLASS.equals(MecanumDrive.class)) {
             MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+
 
             waitForStart();
 
@@ -23,19 +37,12 @@ public final class SplineTest extends LinearOpMode {
                         .splineToConstantHeading(new Vector2d(20, 50), Math.PI / 2)
                         //.splineTo(new Vector2d(0, 60), Math.PI)
                         .build());
-        } else if (TuningOpModes.DRIVE_CLASS.equals(TankDrive.class)) {
-            TankDrive drive = new TankDrive(hardwareMap, beginPose);
-
-            waitForStart();
-
-            Actions.runBlocking(
-                    drive.actionBuilder(beginPose)
-                            .setTangent(0)
-                            .splineTo(new Vector2d(5, 50), Math.PI / 2)
-                            //.splineTo(new Vector2d(0, 60), Math.PI)
-                            .build());
-        } else {
+        }
+        else {
             throw new RuntimeException();
         }
+    }
+    public void armToggle(){
+
     }
 }
